caa67313951b962a5cbf6c9453e2b4e23a1c2dae
Caio Peternela <caiopeternela.dev@gmail.com>
2022-07-06 15:01:18 -0300

    aperfeiçoando o app

--- buscador_app/views.py
+++ buscador_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 import urllib.request, json 
+from urllib.error import HTTPError
 
 
 # Create your views here.
@@ -7,7 +8,7 @@ def index(request):
     return render(request, 'index.html')
 
 def buscador(cep):
-    cep = cep.replace('-','')
+    cep = cep.replace('-','').replace(' ', '').replace('.', '')
     json_cep = 'https://viacep.com.br/ws/' + cep + '/json/'
     with urllib.request.urlopen(json_cep) as url:
         dict_cep = json.loads(url.read().decode())
@@ -18,8 +19,20 @@ def buscador(cep):
         output.append('Cidade: ' + dict_cep['localidade'])
         output.append('Estado: ' + dict_cep['uf'])
         return '\n\n'.join(output)
-        
+
+def deu_erro(cep):
+    cep = cep.replace('-','')
+    return False if len(cep) == 8 and cep.isnumeric() else True
+
+def cep_invalido():
+    return True
+
 def endereco(request):
     if request.method == 'POST':
         cep = request.POST.get('cep')
-    return render(request, 'endereco.html', {'buscador': buscador(cep)})
+    try:
+        return render(request, 'endereco.html', {'buscador': buscador(cep)})
+    except HTTPError:
+        return render(request, 'index.html', {'cep_invalido': cep_invalido()})
+    except KeyError:
+        return render(request, 'index.html', {'cep_invalido': cep_invalido()})
